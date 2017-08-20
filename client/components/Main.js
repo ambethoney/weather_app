9d899a65c4da2ccfd00db7b8dfa7b98cfc8f0999
Angelina Bethoney <ambethoney@yahoo.com>
2017-08-20 17:58:42 -0400

    First commit

---
+++ client/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Main = React.createClass({
+
+  render() {
+    // Then we go ahead and return some JSX
+    return (
+    );
+  }
+
+});
+
+export default Main;
